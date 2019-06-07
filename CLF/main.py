55471258fb70760c252145490709f0c44271f3e1
barbrakr <barbrakr@liv.ac.uk>
2019-06-07 18:35:05 +0200

    conversion

--- CLF/main.py
+++ CLF/main.py
@@ -5,25 +5,9 @@ def main():
     # Read directory of dicom files into a single 3D nifti file.
     
     dicom_directory = input("Please enter folder.")
-    from nipype.interfaces.dcm2nii import Dcm2niix
-    converter = Dcm2niix()
-    converter.inputs.source_dir = dicom_directory
-    converter.inputs.compression = 5
-    converter.inputs.output_dir = dicom_directory
-    converter.cmdline
-    converter.run() 
 
-    from myfunctions import prompt_demographics
+    from myfunctions import prompt_demographics, my_dcm2niix
     age, sex = prompt_demographics()
-
-    #class dicom2nifti.convert_dicom.Vendor
-    #GE = input("Please state vendor - '2' for GE")
-    #dicom_list = dicom2nifti.common.read_dicom_directory(dicom_directory)
-    # TODO: add to dicom_array_to_nifti kwargs to save output and read instead of converting if present.
-    #nifti_dict = dicom2nifti.convert_dicom.are_imaging_dicoms(dicom_list)
-    #dicom2nifti.convert_dicom.dicom_series_to_nifti(original_dicom_directory, output_file='.', reorient_nifti=True)    
-    #nifti_image = nifti_dict['NIFTI']
-    #nifti_file_path = nifti_dict['NII_FILE']
-
+    my_dcm2niix(dicom_directory)
 
 main()    
--- CLF/myfunctions.py
+++ CLF/myfunctions.py
@@ -2,3 +2,16 @@ def prompt_demographics():
    age = input("Please enter age in whole years: ")
    sex = input("Please enter sex (m/f): ")
    return age, sex
+
+def my_dcm2niix(folder):
+    # convert dcm series to nii.gz and .json using nipype and dcm2niix
+    from nipype.interfaces.dcm2nii import Dcm2niix
+    converter = Dcm2niix()
+    converter.inputs.source_dir = folder
+    converter.inputs.compression = 5
+    converter.inputs.output_dir = folder
+    converter.inputs.out_filename = '%i_%4s_%d'
+    converter.cmdline
+    converted_files = converter.run()
+    return converted_files
+

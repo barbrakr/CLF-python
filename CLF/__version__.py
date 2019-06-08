fe9fdc3f893dd6213cf9ee05fc551eb7c4a46000
barbrakr <50030195+barbrakr@users.noreply.github.com>
2019-06-08 21:32:30 +0200

    Update __version__.py

--- CLF/__version__.py
+++ CLF/__version__.py
@@ -1,11 +1,11 @@
 # This file is read by setup.py and executed as a string.
 
-__title__ = "CLF"
-__description__ = "A tool for detecting brain lesions."
+__title__ = "Cortical Lesion Finder"
+__description__ = "A tool for detecting brain lesions within cortex."
 __url__ = "https://github.com/barbrakr/CLF-python"
 __version__ = "0.0.1"
-__author__ = "Barbara Kreilkamp"
-co_authors = ['Devin Crowley','Roopa Kalsank Pai','Samuel Berry','Lennart Wittkuhn','Carsten Schmidt-Samoa']
+__author__ = "Barbara A.K. Kreilkamp"
+co_authors = ['Devin Crowley','Roopa Kalsank Pai','Lennart Wittkuhn','Carsten Schmidt-Samoa','Samuel Berry','Tom Chambers']
 __author_email__ = "barbrakr@liv.ac.uk"
 __license__ = "GNU General Public License v3.0"
 __min_python_version__ = (3, 6)
